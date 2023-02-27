8b9938eb2971668bcf2e8b0fe2b62b23eb0ce260
rldettloff <114546927+rldettloff@users.noreply.github.com>
2023-02-26 19:15:50 -0800

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,19 @@
+class Animal {
+    private int age;
+    private String name;
+
+
+
+}
+
+class Dog extends Animal {
+   
+    public void bark() {
+        System.out.println("bark bark!");
+    }
+
+}
+
+class Jindo extends Dog {
+    private String origin;
+}
---
+++ CourseClass.java
@@ -0,0 +1,25 @@
+class Course {
+    private String courseNumber;
+    private String CourseTitle;
+    
+    void setCourseNumber(string inputCourseNumber) {
+        courseNumber = inputCourseNumber;
+
+    }
+    String getCourseNumber() {
+        return courseNumber;
+    }
+    void printCourseInfo(){
+        System.out.println("course number:" + );
+        System.out.println("course number" + );
+    }
+    void setCourseTitle(string inputCourseTitle) {
+        Coursetitle = inputCourseTitle;
+
+    }
+}
+class OfferedCourse extends CourseClass {
+
+
+}
+
