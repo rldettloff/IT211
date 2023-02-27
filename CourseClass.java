1fbcb3c817904fdbdf85befa490cb8911153d7c8
rldettloff <114546927+rldettloff@users.noreply.github.com>
2023-02-26 19:28:53 -0800

    Add files via upload

--- CourseClass.java
+++ CourseClass.java
@@ -1,8 +1,8 @@
 class Course {
     private String courseNumber;
-    private String CourseTitle;
+    private String courseTitle;
     
-    void setCourseNumber(string inputCourseNumber) {
+    void setCourseNumber(String inputCourseNumber) {
         courseNumber = inputCourseNumber;
 
     }
@@ -10,16 +10,50 @@ class Course {
         return courseNumber;
     }
     void printCourseInfo(){
-        System.out.println("course number:" + );
-        System.out.println("course number" + );
+        System.out.println("course number:" + courseNumber);
+        System.out.println("course number" + courseTitle );
     }
-    void setCourseTitle(string inputCourseTitle) {
-        Coursetitle = inputCourseTitle;
+    void setCourseTitle(String inputCourseTitle) {
+        courseTitle = inputCourseTitle;
 
     }
 }
-class OfferedCourse extends CourseClass {
+class OfferedCourse extends Course {
+    private String instructorName;
+    private String location;
+    private String classTime;
 
+    public void setInstructorName(String name) {
+        instructorName = name;
+    }
 
+    public String getInstructorName() {
+        return instructorName;
+    }
+
+    public void setLocation(String loc) {
+        location = loc;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setClassTime(String time) {
+        classTime = time;
+    }
+
+    public String getClassTime() {
+        return classTime;
+    }
+
+    public void printCourseInfo() {
+        super.printCourseInfo();
+        System.out.println("course number:" + courseNumber);
+        System.out.println("course number" + courseTitle );
+        System.out.println("Instructor's Name: " + instructorName);
+        System.out.println("Location: " + location);
+        System.out.println("Class Time: " + classTime);
+    }
 }
 
